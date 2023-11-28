81c18f864ebf6a5095ebb3d96d8a8492370db2a8
SaiAryan1784 <saiaryan.goswami1784@gmail.com>
2023-11-29 00:52:46 +0530

    Website complete - less goooo

---
+++ src/Components/letsConnect/LetsConnect.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import "./letsconnect.css"
+import DataVerseLogo from "./DataVerseLogo.png"
+import Footer from '../Footer'
+const LetsConnect = () => {
+    return (
+        <div>
+            <div class="bottom">
+                <div class="developed">
+                    <center>
+                        <img src={DataVerseLogo} alt='logo' width="250px" />
+                        <h2>Developed And Powered By Dataverse <br /> The Data Science Club Of ABESEC</h2>
+                    </center>
+                </div>
+                <hr style={{ border: "1px solid gray; opacity: 0.6" }} />
+                <div class="letsConnect">
+                    <div>
+                        <h1><em>Let's</em></h1>
+                        <h1><em><span>connect</span></em></h1>
+                    </div>
+                    <div>
+                        <pre class="lets">If you're curious about us, <br />
+                            we're
+                            ready to answer any and <br />
+                            all
+                            questions.</pre>
+                        <a href="https://www.instagram.com/dataverse.abesec/" target="_blank" rel="noreferrer"><button class="button">Contact US</button></a>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+          
+        </div>
+
+    )
+}
+
+export default LetsConnect
+
+
+
+
+
